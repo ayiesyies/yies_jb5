eef1f09937e140536b79125abd37527a350ba161
ayiesyies <nuraisyahriskijunita@gmail.com>
2024-02-24 10:59:01 +0700

    mengubah gambar

---
+++ project/resources/views/view_home.blade.php
@@ -0,0 +1,31 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+        Perpustakaan Perguruan Tinggi merupakan unit kerja pelaksana teknis (UPT) Perguruan Tinggi yang bersama-sama 
+		dengan unit lain turut melaksanakan Tri Dharma Perguruan Tinggi, yaitu: pendidikan, penelitian, dan pengabdian 
+		kepada masyarakat dengan cara memilah, menghimpun, mengolah, merawat, serta menyebarluaskan sumber informasi kepada 
+		lembaga induknya pada khususnya dan sivitas akademika pada umumnya. Kelima tugas tersebut dilaksanakan dengan 
+		tata cara administrasi dan organisasi yang berlaku bagi penyelenggaraan sebuah perpustakaan.
+    <p>
+    <h4>Perpustakaan PNRI</h4>
+    Perpustakaan Nasional Republik Indonesia (PNRI) adalah lembaga perpustakaan yang berada di bawah naungan 
+    Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi Republik Indonesia. PNRI memiliki peran yang 
+    sangat penting dalam mempromosikan literasi, pengetahuan, dan budaya di Indonesia melalui layanan 
+    perpustakaan yang berkualitas dan berkelanjutan. Sebagai perpustakaan nasional, PNRI menjadi sumber 
+    informasi yang penting bagi berbagai kalangan, mulai dari mahasiswa, peneliti, hingga masyarakat umum.
+
+    <p>
+    Sebagai institusi yang bertanggung jawab atas pengelolaan dan pengembangan perpustakaan nasional 
+    di Indonesia, PNRI terus berupaya untuk menjadi pusat pengetahuan yang dinamis, relevan, dan berkualitas, 
+    serta memainkan peran strategis dalam mendukung pembangunan sosial, budaya, dan ekonomi bangsa.
+
+    <p>
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+
+@endsection
